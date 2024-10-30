7b7904d0ac68449de4c206fa429400ef7ab4c569
Ly <Lyrian014@gmail.com>
2024-10-29 22:20:57 -0300

    att

--- categoria.php
+++ categoria.php
@@ -1,7 +1,9 @@
 <?php
 include "php/conexao.php";
 include "funcao.php";
+$categoria = listar($conn,'produtos');
 
+deletar($conn,'categoria',1);
 
 ?>
 
@@ -12,14 +14,35 @@ include "funcao.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto Lojas Roupas</title>
-    <link rel="stylesheet" href="css/style-prod.css">
+    <link rel="stylesheet" href="css/style-cat.css">
 </head>
 <body>
     <div class="container">
 
         <div class="lista">
-        
-
+        <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome do Produto</th>
+                <th>Categoria</th>
+                <th>Preço</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Camiseta</td>
+                <td>Roupas</td>
+                <td>R$ 50,00</td>
+                <td>
+                    <button class="action-button delete-button">Deletar</button>
+                </td>
+            </tr>
+            <!-- Adicione mais linhas conforme necessário -->
+        </tbody>
+    </table>
         </div>
     </div>
 </body>
